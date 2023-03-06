3f992ef521cb054b3d494e804b35d8fc12988118
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2023-03-06 13:37:19 +0300

    app maven structured

---
+++ src/main/java/com/leonovets/algorithms/AlgorithmsRdbsApplication.java
@@ -0,0 +1,17 @@
+package com.leonovets.algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mikhail.Leonovets
+ * @since 03/06/2023 - 13:30
+ */
+@SpringBootApplication
+public class AlgorithmsRdbsApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(AlgorithmsRdbsApplication.class);
+    }
+
+}
